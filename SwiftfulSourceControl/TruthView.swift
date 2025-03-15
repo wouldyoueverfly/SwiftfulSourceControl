60471ca8deee02797c01692d274bef4bf2459b70
Rsln <cpptypxe@ya.ru>
2025-03-15 15:00:02 -0700

    Untitled Pull Request (#4)
    * new file SettingsView
    
    * [Feature] Add truth logic
    
    * new title

---
+++ SwiftfulSourceControl/TruthView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by dofxmine on 16.03.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @State private var isThereTruth: Bool = false
+    
+    var body: some View {
+        if isThereTruth {
+            Text("Truth")
+        } else {
+            Text("Lie")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
