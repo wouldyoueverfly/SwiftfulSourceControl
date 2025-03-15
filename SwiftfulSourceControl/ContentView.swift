97ec1458f67ae47ddf26c27762e3260dfda6f4b2
Rusln <cpptypxe@ya.ru>
2025-03-15 10:26:16 -0700

    Another commit!

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -14,10 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.pink)
             Text("Git Source Control")
-            
-            Button("Open Source Control") {
-                
-            }
         }
         .padding()
     }
