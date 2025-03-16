7400a2f65669aecfad7edfa1cfe1099ab6ee5a1a
Rusln <cpptypxe@ya.ru>
2025-03-16 02:41:25 -0700

    [Feature] Add truth logic

--- SwiftfulSourceControl/SettingsView.swift
+++ SwiftfulSourceControl/SettingsView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @State private var isThereTruth: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isThereTruth {
+            Text("Truth")
+        } else {
+            Text("Lie")
+        }
     }
 }
 
