cfd86e06f173cc470d86bec98151eec0957fa631
Rusln <cpptypxe@ya.ru>
2025-03-16 02:39:07 -0700

    new file SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by dofxmine on 16.03.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
