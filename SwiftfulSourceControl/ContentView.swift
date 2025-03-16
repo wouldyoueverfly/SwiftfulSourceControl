04d6ac26733ff6f86930723d478d40646f337e69
Rusln <cpptypxe@ya.ru>
2025-03-15 23:45:41 -0700

    [Feature] Change heart.fill to bolt.fill

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 20) {
-            Image(systemName: "heart.fill")
+            Image(systemName: "bolt.fill")
                 .imageScale(.large)
                 .foregroundStyle(.pink)
             Text("Stay coding addicted")
